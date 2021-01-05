4c3e07d7e9a1877f2e43d6622cb1e044a82ebced
CRudorf <77019785+CRudorf@users.noreply.github.com>
2021-01-05 17:02:16 -0500

    Added main and helper files.

---
+++ helpers.py
@@ -0,0 +1,270 @@
+# Standard Libraries
+import requests
+import csv
+import os
+import glob
+import zipfile
+import io
+import math
+import pickle
+import yaml
+import sys
+from datetime import datetime as dt
+from dateutil.relativedelta import relativedelta
+
+# Third Party Libraries
+import pandas as pd
+
+# NOTE: Logging has been changed to print statements specifically for sharing on Github.
+
+def load_yaml_config(filename: str="config.yaml") -> dict:
+    with open(filename, 'r') as stream:
+        config = yaml.load(stream)
+    return config
+
+        
+def utc_to_local(x, timezone) -> pd.Timestamp:
+    '''
+    :param timestamp object (pandas or datetime.datetime):
+    :param timezone (pandas accepted timezone eg. Us/Pacific):
+    :return: timestamp object
+    '''
+    return pd.Timestamp(x).tz_localize('utc').tz_convert(timezone).replace(tzinfo=None)
+
+
+def timestamp_string(ts, formatstr='%Y-%m-%dT%H:%M') -> pd.Timestamp:
+    '''
+    :param ts: utc timestamp string
+    :param formatstr: Converts a UTC YYYY-MM-DD or other into local time
+    :return: timestamp
+    '''
+    try:
+        return utc_to_local(pd.to_datetime(ts, errors='coerce', format=formatstr), 'US/Pacific')
+    except Exception as e:
+        print(e); print(ts)
+        return None
+
+
+def determine_timestamps(df: pd.DataFrame, start: pd.Timestamp, end: pd.Timestamp) -> (pd.Timestamp, pd.Timestamp):
+    start = start.date()
+    end = end.date()
+    data_start = df.index.min()
+    data_end = df.index.max()
+    if pd.Timestamp(start).date() < pd.Timestamp(data_start).date():
+        print(start)
+        print(data_start)
+    else:
+        start = pd.Timestamp(data_end).date() + relativedelta(days=1)
+    return start, end
+
+
+def daily_peaks(df: pd.DataFrame, col: bool=None, hb: int=7, he: int=22) -> pd.DataFrame:
+    if col is None:
+        col = df.columns[0]
+    start = df.index.min().date()
+    end = df.index.max().date()
+    df['Hour'] = [x.hour for x in df.index]
+    results = pd.DataFrame()
+
+    while start <= end:
+        subdf = pd.DataFrame(df[col].loc[(df.index >= start) & (df.index < end)])
+        onp = subdf.loc[(subdf['Hour'] >= hb) & (subdf['Hour'] <= he)]
+        offp = subdf.loc[(subdf['Hour'] < hb) | (subdf['Hour'] > he)]
+        results.append(pd.DataFrame([[onp.mean(), offp.mean()]], index=[start]))
+        start = start + relativedelta(days=1)
+
+    results.columns = [col + pktype for pktype in ['On Peak', 'Off Peak']]
+    return results
+
+
+def split_dates_by_limit(start_date: str, end_date: str, limit: int, date_format: str="%m/%d/%y") -> dict:
+    '''
+
+    APIs commonly have date limits for historical data. This function takes a desired start and end date, and returns
+    a dictionary of start and end dates that are generated based on the limit of the API.
+
+    :param start_date: str
+    :param end_date: str
+    :param limit: int
+    :param date_format: str
+    :return: dictionary if splits, or None if no split required.
+
+    For example, YES API has a 90 day period limit when calling their API. CAISO OASIS has a 30 day period.
+
+    For a YES call, an example call would look like:
+    split_dates_by_limit(start_date='01/01/17', end_date='12/31/18', limit=90)
+    expected return:
+    {'start_dates': ['01/01/17', '04/01/17', '06/30/17', '09/28/17', '12/27/17', '03/27/18', '06/25/18', '09/23/18',
+    '12/22/18'],
+    'end_dates': ['03/31/17', '06/29/17', '09/27/17', '12/26/17', '03/26/18', '06/24/18', '09/22/18', '12/21/18',
+    '12/31/18']}
+    '''
+
+    # Convert limit to pd.Timedelta object for easy comparison
+    limit = pd.Timedelta(days=limit)
+
+    # Convert str date to given date_format and calculate delta between start and end date
+    dt_start_date = pd.to_datetime(start_date, format=date_format)
+    dt_end_date = pd.to_datetime(end_date, format=date_format)
+    dt_delta = dt_end_date - dt_start_date
+
+    # if the delta is less than limit, return 1 to indicate only 1 call is needed to API
+    if limit > dt_delta:
+        return None
+    else:
+        # Else, set up necessary split start and end dates
+        new_start_date = dt_start_date + limit
+        new_end_date = dt_start_date + limit - pd.Timedelta(1)
+
+        # Lists as containers for new start and end dates to iterate through, initializing with original start_date and
+        # end date is equal to original start date + limit
+        start_dates = [dt_start_date]
+        end_dates = [new_end_date]
+
+        # Number of calls needed by dividing the delta over given limit, and rounding up
+        splits = math.ceil(dt_delta / limit)
+
+        # Number of start dates must be equal to number of splits
+        try:
+            while len(start_dates) < splits:
+                # Append new start date, then add limit for next iteration
+                start_dates.append(new_start_date)
+                new_start_date += limit
+
+                # if calculated end date is less than original end date & original end date - calc end date > limit, we
+                # keep appending to end dates list.
+                if new_end_date < dt_end_date and (dt_end_date - new_end_date > limit):
+                    end_dates.append(new_end_date + limit)
+                    new_end_date += limit
+                # Else the last end date appended will be last calced end date + remaining delta
+                else:
+                    end_dates.append(new_end_date + (dt_end_date - new_end_date))
+        except Exception as e:
+            print('Critical', 'Failed to split start and end dates for API calls.')
+
+    # List comprehension to convert datetime objects back to string types within same list containers, then return lists
+    start_dates = [x.strftime(date_format) for x in start_dates]
+    end_dates = [x.strftime(date_format) for x in end_dates]
+
+    # Return a dictionary of start_dates and end_dates list
+    return {'start_dates': start_dates, 'end_dates': end_dates}
+
+
+def read_pickle(name: str, sub_folder: str='pickles'):
+    if sub_folder is not None:
+        with open(f'{sub_folder}/{name}.pkl', 'rb') as file:
+            df = pickle.load(file)
+    else:
+        with open(f'{name}.pkl', 'rb') as file:
+            df = pickle.load(file)
+    return df
+
+
+def write_pickle(df: pd.DataFrame, name: str, sub_folder: str='pickles'):
+    if sub_folder is not None:
+        mkdir(sub_folder)
+        with open(f'{sub_folder}/{name}.pkl', 'wb') as output_file:
+            pickle.dump(df, output_file)
+        return f'Wrote {sub_folder}/{name}.pkl'
+    else:
+        with open(f'{name}.pkl', 'wb') as output_file:
+            pickle.dump(df, output_file)
+        return f'Wrote {name}.pkl'
+
+
+def mkdir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def rmdir(directory):
+    if os.path.exists(directory):
+        try:
+            os.removedirs(directory)
+        except Exception as e:
+            print(e)
+
+
+def get_latest_csv(filepath: str, delete=False) -> str:
+    list_of_files = glob.glob('%s/*.csv' % filepath)
+    try:
+        latest_file = max(list_of_files, key=os.path.getctime)
+        df = pd.read_csv(latest_file)
+        if delete:
+            try:
+                os.remove(latest_file)
+            except Exception as e:
+                print(e)
+    except ValueError as e:
+        print(e)
+        if e == 'max() arg is an empty sequence':
+            return print('no csv found')
+    return df
+
+
+def merge_dataframes(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    if df1.empty:
+        result = df2
+    elif df2.empty:
+        result = df1
+    else:
+        result = pd.merge(df1, df2, how='outer', left_index=True, right_index=True)
+    return result
+
+def add_days_to_date(date: str, add: int, format: str="%Y%m%d") -> str:
+    dt_date = pd.Timestamp(date)
+    dt_date += pd.Timedelta(days=add)
+    new_str_date = dt.strftime(dt_date, format)
+    return new_str_date
+
+class Web(object):
+    def __init__(self, url=None, payload=None):
+        self.session = requests.Session()
+        try:
+            # Proxy obfuscated for security reasons; Normally stored in ecrypted configuration file.
+            self.proxy = {"http": "placeholder",
+                          "https": "placeholder"}
+            self.session.proxies.update(self.proxy)
+        except Exception as e:
+            print('Backup proxy invoked from pullback')
+            #TODO find a better backup
+            # self.proxy = {"http": backup_http_proxy,
+            #               "https": backup_https_proxy}
+
+        if url is not None:
+            self.url = url
+            if payload is not None:
+                try:
+                    self.payload = payload
+                    self.r = self.session.get(url, params=self.payload)
+                except Exception as e:
+                    print(e)
+            else:
+                self.r = self.session.get(url)
+
+    def unzip(self, sub_folder_name=None):
+
+        mkdir("unzipped")
+        self.sub_folder_name = sub_folder_name
+        if self.sub_folder_name is not None:
+            try:
+                mkdir(f"unzipped/{self.sub_folder_name}")
+                with zipfile.ZipFile(io.BytesIO(self.r.content), "r") as zip_ref:
+                    zip_ref.extractall("unzipped/%s" % self.sub_folder_name)
+            except Exception as e:
+                print(e)
+        else:
+            try:
+                with zipfile.ZipFile(io.BytesIO(self.r.content), "r") as zip_ref:
+                    zip_ref.extractall("unzipped")
+            except Exception as e:
+                print(e)
+
+    def get_text(self):
+        return self.r.text
+
+    def get_content(self):
+        return self.r.content
+
+    def get_json(self):
+        return self.r.json()
---
+++ main.py
@@ -0,0 +1,233 @@
+# Standard Library
+import time
+import pickle
+
+# Third Party Libraries
+import pandas as pd
+import sqlalchemy
+
+# Local libraries
+import helpers
+from log import log, err_handle
+
+#functions.load_configuration(description=True)
+log.log.name = 'Oasis'
+
+transmission_lines = ['NOB_ITC', 'ADLANTOVICTVL-SP_ITC', 'ELDORADO_ITC', 'MALIN500', 'MCCLMKTPC_ITC', 'MEAD_ITC',
+                      'PATH15_BG', 'PALOVRDE_ITC', 'PATH26_BG', 'VICTVL_ITC']
+
+
+class TransmissionData:
+
+    def __init__(self, ti_id: str, start_date: str, end_date: str, ti_direction: str = 'ALL', sub_folder: str = None):
+        self.ti_id = ti_id
+        self.start_date = start_date
+        self.end_date = end_date
+        self.ti_direction = ti_direction
+        self.sub_folder = sub_folder
+
+    def get_current_transmission(self, insert=False):
+        """
+
+        Downloads a zip file from CAISO OASIS Current Transmission Usage, and unzips file to CSV
+
+        example oasis url: http://oasis.caiso.com/oasisapi/SingleZip?resultformat=6&queryname=TRNS_CURR_USAGE&version=1&
+        ti_id=PALOVRDE_ITC&ti_direction=ALL&startdatetime=20181105T07:00-0000&enddatetime=20181106T07:00-0000
+
+        """
+        base_url = r'http://oasis.caiso.com/oasisapi/SingleZip?resultformat=6&queryname=TRNS_CURR_USAGE&version=1'
+        timestamp_adder = r'T08:00-0000'
+
+        payload = {'ti_id': self.ti_id, 'ti_direction': self.ti_direction, 'startdatetime': self.start_date + timestamp_adder,
+                   'enddatetime': self.end_date + timestamp_adder}
+        w = helpers.Web(base_url, payload=payload)
+
+        if self.sub_folder is not None:
+            try:
+                w.unzip(sub_folder_name=self.sub_folder)
+                log.write('debug',
+                          'successful unzip in sub_folder_name check in oasis.get_current_transmission for ti_id:'
+                          + ' %s' % self.ti_id)
+            except Exception as e:
+                log.write('critical',
+                          'failed unzip in sub_folder_name check in oasis.get_current_transmission for ti_id:'
+                          + ' %s' % self.ti_id, err=err_handle(e, __file__))
+        else:
+            try:
+                w.unzip()
+                log.write('debug', 'successful unzip in else check in oasis.get_current_transmission for ti_id:'
+                          + ' %s' % self.ti_id)
+            except Exception as e:
+                log.write('critical',
+                          'failed unzip in sub_folder_name check in oasis.get_current_transmission for ti_id:'
+                          + ' %s' % self.ti_id, err=err_handle(e, __file__))
+
+        df = helpers.get_latest_csv('unzipped\%s' % self.sub_folder)
+        if insert:
+            self.insert_to_mysql(df)
+
+        return df
+
+    @staticmethod
+    def remove_transmission_types(df, col='LABEL', condition1='Constraint', condition2='Hourly TTC'):
+        '''
+        Specific function created to filter to particular transmission types that was needed for use case.
+
+        :param df: pd.DataFrame of data with condition types.
+        :param col: str, column name.
+        :param condition1: str, first condition.
+        :param condition2: str, second condition.
+        :return: pd.DataFrame with unnecessary transmission types removed.
+        '''
+
+        if condition2 is None:
+            df = df[df[col] == condition1]
+        else:
+            df = df[(df[col] == condition1) | (df[col] == condition2)]
+        return df
+
+    @staticmethod
+    def insert_to_mysql(df):
+        # credentials below are made up; standard practice would store encrypted credentials, and pass via reading a config file
+        local_mysql_server, local_mysql_username, local_mysql_password, local_mysql_database = \
+            'localhost', 'root', 'root', 'duck'
+
+        col_data_types = {'OPR_DT': sqlalchemy.types.DATE, 'OPR_HR': sqlalchemy.types.INT,
+                          'INTERVALSTARTTIME_GMT': sqlalchemy.types.CHAR(255),
+                          'INTERVALENDTIME_GMT': sqlalchemy.types.CHAR(255),
+                          'TI_ID': sqlalchemy.types.NVARCHAR(255),
+                          'TI_DIRECTION': sqlalchemy.types.NVARCHAR(255),
+                          'MARKET_RUN_ID': sqlalchemy.types.NVARCHAR(255),
+                          'TI_CONSTRAINT_ID': sqlalchemy.types.NVARCHAR(255),
+                          'TR_TYPE': sqlalchemy.types.NVARCHAR(255),
+                          'XML_DATA_ITEM': sqlalchemy.types.NVARCHAR(255),
+                          'LABEL': sqlalchemy.types.NVARCHAR(255),
+                          'POS': sqlalchemy.types.INT, 'OPR_INTERVAL': sqlalchemy.types.INT,
+                          'MW': sqlalchemy.types.FLOAT,
+                          'MARKET_RUN_ID': sqlalchemy.types.NVARCHAR(255),
+                          'GROUP': sqlalchemy.types.INT}
+
+        with Database('local_mysql') as db:
+            table_name = 'db_test_trans'
+            try:
+                df.to_sql(name=table_name, con=db.engine, if_exists='append', schema=local_mysql_database,
+                          dtype=col_data_types)
+                log.write('debug', 'sql written to table %s' % table_name)
+            except Exception as e:
+                log.write('critical',
+                          'failed to insert to table %s' % table_name, err=err_handle(e, __file__))
+
+
+class Oasis:
+    oasis_url = r'http://oasis.caiso.com/oasisapi/SingleZip?resultformat=6&queryname=%s&version=1'
+
+    def __init__(self, start_date: str, end_date: str):
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def get(self, call_type: str='Demand'):
+        """
+        Downloads a zip file from CAISO OASIS System Demand for all or renewable demand, and unzips file to CSV
+
+        example oasis url: http://oasis.caiso.com/oasisapi/SingleZip?resultformat=6&queryname=SLD_REN_FCST&version=1
+        &startdatetime=20181101T07:00-0000&enddatetime=20181110T08:00-0000
+
+        Example call:
+        x = Oasis("20180101", "20180201").get_system_demand(renewable=True)
+        x.get_system_demand() or x.get_system_demand(renewable=True)
+        """
+        #TODO add more types of calls here other than demand and renewables; add exception handling on invalid types instead of else
+        if call_type == 'Renewable':
+            base_url = self.oasis_url % 'SLD_REN_FCST'
+        else:
+            base_url = self.oasis_url % 'SLD_FCST'
+
+        timestamp_adder = r'T08:00-0000'
+        dates = helpers.split_dates_by_limit(start_date=self.start_date, end_date=self.end_date, limit=30,
+                                             date_format='%Y%m%d')
+        df = pd.DataFrame()
+
+        if dates is None:
+            payload = {'startdatetime': self.start_date + timestamp_adder,
+                       'enddatetime': self.end_date + timestamp_adder}
+            log.write('debug', f'Attempting to call OASIS for {call_type} for {self.start_date}-{self.end_date}.'
+            f'This can take awhile.')
+            w = helpers.Web(base_url, payload=payload)
+
+            try:
+                w.unzip()
+                log.write('debug', 'successful unzip in oasis.get_system_demand date range'
+                          + ' %s-%s' % (self.start_date, self.end_date))
+            except Exception as e:
+                log.write('critical',
+                          'failed unzip in oasis.get_system_demand for date range'
+                          + ' %s-%s' % (self.start_date, self.end_date), err=err_handle(e, __file__))
+
+            df = helpers.get_latest_csv('unzipped', delete=True)
+        else:
+            start_dates = dates.get('start_dates', 'start_dates not found.')
+            end_dates = dates.get('end_dates', 'end_dates not found.')
+
+            if isinstance(start_dates, (list,)) and isinstance(end_dates, (list,)):
+                for s_date, e_date in zip(start_dates, end_dates):
+                    payload = {'startdatetime': s_date + timestamp_adder,
+                               'enddatetime': e_date + timestamp_adder}
+                    try:
+                        log.write('debug',
+                                  f'Attempting to call OASIS for {call_type} for {self.start_date}-{self.end_date}.'
+                                  f'This can take awhile.')
+                        w = helpers.Web(base_url, payload=payload)
+                        w.unzip()
+                        log.write('debug',
+                                  'successful unzip in oasis.get_system_demand date range'
+                                  + ' %s-%s' % (s_date, e_date))
+                        if len(df) == 0:
+                            df = helpers.get_latest_csv('unzipped', delete=True)
+                            log.write('debug', f'Found most recent CSV unzipped from CAISO Oasis for {s_date}-{e_date}. '
+                            f'Creating new DF.')
+                        else:
+                            temp_df = helpers.get_latest_csv('unzipped', delete=True)
+                            df = df.append(temp_df)
+                            log.write('debug', f'successfully appended {s_date} - {e_date} to existing DF.')
+                    except Exception as e:
+                        log.write('critical',
+                                  'failed unzip in oasis.get_system_demand for date range'
+                                  + ' %s-%s' % (s_date, e_date), err=err_handle(e, __file__))
+        try:
+            helpers.rmdir('unzipped')
+        except Exception as e:
+            log.write('debug', 'Unable to remove unzipped directory.', err=err_handle(e, __file__))
+        return df
+
+
+# if __name__ == '__main__':
+#     ti_id = 'PALOVRDE_ITC'
+#     start_date = '20190129'
+#     end_date = '20190130'
+#     ti_direction = 'ALL'
+#     print(TransmissionData(ti_id, start_date, end_date, sub_folder=ti_id).get_current_transmission())
+#
+#     x = Oasis("20180101", "20180301").get_system_demand(renewable=True)
+#     print(x)
+
+
+#
+# while i <= weeks:
+#     for x in transmission_lines:
+#         a = TransmissionData(x, start_date, end_date, sub_folder=x)
+#         a.get_current_transmission()
+#         print("%s has attempted to be unzipped" % x)
+#         time.sleep(5)
+#     start_date = functions.add_days_to_date(start_date, 7)
+#     end_date = functions.add_days_to_date(end_date, 7)
+#     i += 1
+
+# while i <= weeks:
+#     for x in transmission_lines:
+#         a = TransmissionData(x, start_date, end_date, sub_folder=x)
+#         a.get_current_transmission()
+#         print("%s has attempted to be unzipped" % x)
+#         time.sleep(5)
+#     start_date = functions.add_days_to_date(start_date, 7)
+#     end_date = functions.add_days_to_date(end_date, 7)
+#     i += 1
